728f2a4e699a5b7f60741f2f59a59d724768f160
Ethan O'Quin <ethan.a.oquin@protonmail.com>
2023-04-24 01:14:05 -0500

    Integrate key pair generation into build system

--- deployment/gen_keypair.c
+++ deployment/gen_keypair.c
@@ -4,31 +4,49 @@
 #include "./lib/libhydrogen/hydrogen.h"
 
 // Generate a public/secret key pair for feature package signing and
-// authentication
-// The secret key is used when signing features during packaging,
-// while the public key is held by the car and used to verify feature
+// authentication.
+//
+// The secret key is used when signing features during
+// packaging, while the public key is held by the car and used to verify feature
 // authenticity.
+//
+// Secret key saved as plain hex string, public key split bytewise
+// in "0xXX, " format for use in C headers.
 
-int main(void) {
-  char context[] = "genkey";
-  hydro_sign_keypair feature_authentication_keypair;
-
-  hydro_init();
-
-  hydro_sign_keygen(&feature_authentication_keypair);
-
-  char secretkey[500];
-
-  hydro_bin2hex(secretkey, 500, feature_authentication_keypair.sk,
-                hydro_sign_SECRETKEYBYTES);
-
-  printf("%s", secretkey);
-
-  printf("\n\n");
-
-  for (int i = 0; i < hydro_sign_PUBLICKEYBYTES; i++) {
-    printf("0x%x,", feature_authentication_keypair.pk[i]);
+int main(int argc, char **argv) {
+  // Check args
+  if (argc < 3) {
+    fprintf(stderr, "ERROR: Must provide output filenames for secret key and "
+                    "public key.\n");
+    return 1;
   }
 
-  printf("\n");
+  // Open files
+  FILE *secret_key_file = fopen(argv[1], "w");
+  FILE *public_key_file = fopen(argv[2], "w");
+
+  // Initialize libhydrogen
+  char context[] = "genkey";
+  hydro_sign_keypair feature_authentication_keypair;
+  hydro_init();
+
+  // Generate a key pair for signing
+  hydro_sign_keygen(&feature_authentication_keypair);
+
+  // Save secret key to file
+  char secret_key_str[hydro_sign_SECRETKEYBYTES * 2 + 1];
+  hydro_bin2hex(secret_key_str, 500, feature_authentication_keypair.sk,
+                hydro_sign_SECRETKEYBYTES);
+  fprintf(secret_key_file, "%s\n", secret_key_str);
+
+  // Save public key to file
+  for (int i = 0; i < hydro_sign_PUBLICKEYBYTES; i++) {
+    fprintf(public_key_file, "0x%x,", feature_authentication_keypair.pk[i]);
+  }
+
+  fprintf(public_key_file, "\n");
+
+  // Close files
+  fclose(secret_key_file);
+  fclose(public_key_file);
 }
--- host_tools/sign_feature.c
+++ host_tools/sign_feature.c
@@ -1,33 +1,58 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "./lib/libhydrogen/hydrogen.h"
 
-// Sign a feature packet using the provided private key
-// First arg is secret key, second is feature number, third is car id
-int main(int argc, char **argv) {
-  char context[] = "genkey";
-  hydro_sign_keypair feature_authentication_keypair;
+typedef struct {
+  uint64_t car_id;
+  uint8_t feature_num;
+} __attribute__((packed)) FEATURE_PACKAGE;
 
+typedef struct {
+  FEATURE_PACKAGE p;
+  uint8_t signature[hydro_sign_BYTES];
+} __attribute__((packed)) SIGNED_FEATURE_PACKAGE;
+
+// Sign a feature package using the provided private key.
+//
+// First arg is car ID,
+// second is feature number,
+// third is secret key filename,
+// fourth is output package filename.
+int main(int argc, char **argv) {
+  // Check args
+  if (argc < 4) {
+    fprintf(stderr, "ERROR: Must provide car ID, feature number, secret key "
+                    "filename, and output package filename.\n");
+    return 1;
+  }
+
+  // Initialize libhydrogen
+  char context[] = "signing";
+  hydro_sign_keypair feature_authentication_keypair;
   hydro_init();
 
+  // Load signing secret key
+  FILE *secret_key_file = fopen(argv[3], "r");
+  char input_buffer[1024];
+  fgets(input_buffer, 1024, secret_key_file);
   hydro_hex2bin(feature_authentication_keypair.sk, hydro_sign_SECRETKEYBYTES,
-                argv[1], strlen(argv[1]), 0, 0);
+                input_buffer, strlen(input_buffer) - 1, 0, 0);
 
-  uint64_t feature_packet[2];
+  // Initialize feature package
+  SIGNED_FEATURE_PACKAGE signed_feature;
+  signed_feature.p.car_id = strtoull(argv[1], 0, 10);
+  signed_feature.p.feature_num = strtol(argv[2], 0, 10) & 0xFF;
 
-  feature_packet[0] = strtoull(argv[2], 0, 10);
-  feature_packet[1] = strtoull(argv[3], 0, 10);
-
-  uint8_t signature[hydro_sign_BYTES];
-
-  hydro_sign_create(signature, feature_packet, sizeof(feature_packet), context,
+  // Create signature
+  hydro_sign_create(signed_feature.signature, &signed_feature.p,
+                    sizeof(FEATURE_PACKAGE), context,
                     feature_authentication_keypair.sk);
 
-  char output[512];
-
-  hydro_bin2hex(output, 512, signature, hydro_sign_BYTES);
-
-  printf("%s\n", output);
+  // Output to file
+  FILE *output_file = fopen(argv[4], "wb");
+  fwrite(&signed_feature, sizeof(SIGNED_FEATURE_PACKAGE), 1, output_file);
+  fclose(output_file);
 }
